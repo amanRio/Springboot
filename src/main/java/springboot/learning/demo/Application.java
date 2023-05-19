9b46935626283d775ed83f5e574d42d6a53382ab
amanRio <108719073+amanRio@users.noreply.github.com>
2023-05-19 15:47:36 +0200

    firstsssssghhgh

--- src/main/java/springboot/learning/demo/Application.java
+++ src/main/java/springboot/learning/demo/Application.java
@@ -14,7 +14,7 @@ public class Application {
 	}
    @GetMapping("api/ben")
    public String test(){
-        return("hellow world amanuel dess and it is fucked up");
+        return("hellow world amanuel dess and it is fucked uppp");
    }
 }
 
